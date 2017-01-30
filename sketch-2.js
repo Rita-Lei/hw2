f2b524a3cff35cb032537b49509c04f728a1c8df
Rita-Lei <ylei@cca.edu>
2017-01-29 17:29:16 -0800

    Update sketch-2.js

--- sketch-2.js
+++ sketch-2.js
@@ -7,5 +7,10 @@ function draw() {
 	ellipse(mouseX,mouseY,100,100);
 	ellipse(mouseX-20,mouseY-8,20,20);
 	ellipse(mouseX+20,mouseY-8,20,20);
+	if (mouseX > width/2){	
 	arc(mouseX,mouseY+10,60, 60, 0, PI);
+	} 
+else{
+	arc (mouseX, mouseY+35, 60, 60, PI, PI);	
+	}
 }
