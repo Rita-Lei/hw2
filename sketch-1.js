7f24233c5b95a5984ac00eab6bd2246e79520835
Rita-Lei <ylei@cca.edu>
2017-01-29 16:51:21 -0800

    Create sketch-1.js

---
+++ sketch-1.js
@@ -0,0 +1,11 @@
+function setup() { 
+  createCanvas(400, 400);
+} 
+
+function draw() { 
+  background(220);
+	ellipse(mouseX,mouseY,100,100);
+	ellipse(mouseX-20,mouseY-8,20,20);
+	ellipse(mouseX+20,mouseY-8,20,20);
+	arc(mouseX,mouseY+10,60, 60, 0, PI);
+}
